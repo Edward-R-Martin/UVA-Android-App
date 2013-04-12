a294408a41e4dbc0f9a4a0f21431e6eb85c0404e
erm9b_uva <erm9b@virginia.edu>
2013-04-11 17:49:36 -0700

    testing update

--- UVATour/src/edu/virginia/CS2110/uvatour/InfoScreen.java
+++ UVATour/src/edu/virginia/CS2110/uvatour/InfoScreen.java
@@ -1,11 +1,15 @@
 package edu.virginia.CS2110.uvatour;
 
 import android.widget.Button;
+import android.widget.EditText;
 import sofia.app.Screen;
 
 public class InfoScreen extends Screen
 {
 	private Button finish;
+	private EditText updateScore;
+	private EditText currentCoords;
+	
 	
 	public void finishClicked() {
 		finish();
