afe2a8ec2fb3f5217c12fa7d11e587addc2aea99
erm9b_uva <erm9b@virginia.edu>
2013-04-17 21:22:28 -0400

    update to manifest

--- UVATour/src/edu/virginia/CS2110/uvatour/Map.java
+++ UVATour/src/edu/virginia/CS2110/uvatour/Map.java
@@ -10,12 +10,23 @@ import android.widget.SlidingDrawer;
 import android.widget.Toast;
 import android.widget.SlidingDrawer.OnDrawerCloseListener;
 import android.widget.SlidingDrawer.OnDrawerOpenListener;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
+
 
 @SuppressWarnings("deprecation")
-public class Map extends Screen implements OnClickListener{
+public class Map extends Screen implements OnClickListener, LocationListener {
 
 	private SlidingDrawer information;
 	private Button handle;
+	private TextView latituteField;
+	private TextView longitudeField;
+	private LocationManager locationManager;
+	private String provider;
 	
 	@Override
 	public void onClick(View arg0) {
@@ -25,8 +36,51 @@ public class Map extends Screen implements OnClickListener{
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
- 
         setContentView(R.layout.map);
+        
+        latituteField = (TextView) findViewById(R.id.TextView02);
+        longitudeField = (TextView) findViewById(R.id.TextView04);
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+          System.out.println("Provider " + provider + " has been selected.");
+          onLocationChanged(location);
+        } else {
+          latituteField.setText("Location not available");
+          longitudeField.setText("Location not available");
+        }
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
